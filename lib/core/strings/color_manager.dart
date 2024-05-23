1533f92376484ddeecc93bea075cb76daac6621b
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-05-23 17:38:07 +0300

    delete color not use it from  AppColor

--- lib/core/strings/color_manager.dart
+++ lib/core/strings/color_manager.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  // static Color kColorIconsNavigation = Color(0XFF2D2D2D);
-
   static Color kSecondaryColor = Color(0xffF2F2F2);
   static Color kPrimaryColor = Color(0xffFF6F00);
   static Color kPrimaryDarkColor = Color(0xff2B2D42);
   static Color kSecondaryDarkColor = Colors.blueGrey;
   static Color kErroreBorderColor = Colors.red;
   static Color kErroreFoucaseBorderColor = Color.fromARGB(255, 94, 17, 11);
-
   static Color kColorBlack = Colors.black;
   static Color kColorWhite = Colors.white;
   static Color kColorTransparent = Colors.transparent;
