9a2531db652e2e913279f18b8c69618ed302a74f
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-06-13 12:13:09 +0300

    add permission function and add firebase core and messaging and add function permission in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_2/core/function/get_permission_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,6 +9,7 @@ import 'injection/injection_container.dart' as di;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
+  GetPermissionMessaging();
   SharedPreferences.getInstance().then(
     (prefs) {
       AppSharedPreferences.init(prefs);
