7bf008463f064182254d1b4956f47ae1f03a553f
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-06-22 11:29:40 +0300

    change path en and ar file in pubspec becu change name file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_2/core/function/get_permission_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,6 +9,7 @@ import 'injection/injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await di.init();
   GetPermissionMessaging();
   SharedPreferences.getInstance().then(
