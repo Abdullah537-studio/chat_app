919612e4e841fb5f267ec038e0bfb05c1680e631
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-07-14 14:26:52 +0300

    change name sheck remember me (navigation after splach view )

--- lib/core/function/main_functions/sheck_remember_me_.dart
+++ lib/core/function/main_functions/sheck_remember_me_.dart
@@ -2,7 +2,7 @@ import 'package:chat_2/core/shared/shared_pref.dart';
 import 'package:chat_2/core/strings/route_named_screens_string.dart';
 import 'package:flutter/material.dart';
 
-sheckRememberMeToNavigatorAndGiveTimber(BuildContext context) {
+sheckIfLoginUserPeforeToNavigation(BuildContext context) {
   Future.delayed(
     const Duration(seconds: 3),
     () async {
