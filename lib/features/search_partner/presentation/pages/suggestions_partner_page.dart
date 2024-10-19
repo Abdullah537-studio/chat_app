a9836cbdc7bf2adbf2e934e31020aad82b625f16
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-10-19 11:45:54 +0300

    remove import not use it

--- lib/features/search_partner/presentation/pages/suggestions_partner_page.dart
+++ lib/features/search_partner/presentation/pages/suggestions_partner_page.dart
@@ -1,7 +1,6 @@
 import 'package:chat_2/core/enum/tab_bar_state.dart';
 import 'package:chat_2/core/feature_button_nav_bar/presentation/bootom_navigation_tab_bar_cubit/bootom_tab_bar_cubit.dart';
 import 'package:chat_2/core/strings/image_png.dart';
-import 'package:chat_2/features/chat_partner/presentation/cubits/chat_dialog_cubit/chat_dialog_cubit.dart';
 import 'package:chat_2/features/chat_partner/presentation/widgets/custom_suggestion_partner_info.dart';
 import 'package:chat_2/features/chat_partner/presentation/classes/user_and_partner_info.dart';
 import 'package:chat_2/features/search_partner/data/models/response/search_partner_response_model.dart';
